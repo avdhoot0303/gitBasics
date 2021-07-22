cf22893b52e0865629ccc8862a07032b2d61680e
avdhoot0303 <avdhootpatil245@gmail.com>
2021-07-22 12:29:57 +0530

    changed message in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class Sum{
 	public static void main(String args[]){
 		int a = 20, b =23,sum;
 		sum = a+b;
-		System.out.println("sum:"+sum);
+		System.out.println("sum of a and  b is:"+sum);
 	}
 }
